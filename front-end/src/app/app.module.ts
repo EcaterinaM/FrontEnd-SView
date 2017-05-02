5404d7c242ad4e1bc3d44a01536bf68bd11da815
EcaterinaM <ecaterina.manolache96@gmail.com>
2017-05-02 14:58:47 +0200

    Made navbar Made footer Made Homepage version 1.0 Made Transport Document page version 1.0

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,17 +5,30 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TransportDocComponent } from './components/transportDoc/transportDoc.component';
+import { HomeComponent } from './components/home/home.component';
+import { routing } from './app.routing';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    TransportDocComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    routing,
     HttpModule,
     BrowserAnimationsModule,
     MaterialModule.forRoot(),
+    NgxDatatableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
