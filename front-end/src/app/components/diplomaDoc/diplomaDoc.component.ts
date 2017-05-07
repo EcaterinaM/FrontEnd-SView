9be2d667fca78063c388b63e376c7265e6f9704e
EcaterinaM <ecaterina.manolache96@gmail.com>
2017-05-08 00:19:14 +0200

    Added first version of popup to all pages

--- front-end/src/app/components/diplomaDoc/diplomaDoc.component.ts
+++ front-end/src/app/components/diplomaDoc/diplomaDoc.component.ts
@@ -32,4 +32,21 @@ export class DiplomaDocComponent implements OnInit {
   }
 
 
+  /**
+   * Pentru popup
+   * @type {boolean}
+   */
+  display1: boolean = false;
+  display2: boolean = false;
+
+  showDialog1() {
+    console.log('e aici');
+    this.display1 = true;
+  }
+
+  showDialog2() {
+    console.log('e aici');
+    this.display2 = true;
+  }
+
 }
